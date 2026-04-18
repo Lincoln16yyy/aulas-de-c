fd96113f2d38dd4b7ccaefa7590f6b02321d4485
Lincoln16yyy <kakuoriginal3@outlook.com>
2026-04-18 11:50:50 -0300

    Adicionando código da atividade em C

---
+++ atividade_de_c.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    float l, c, a;
+
+    printf("largura: ");
+    scanf("%f", &l);
+
+    printf("comprimento: ");
+    scanf("%f", &c);
+
+    a = l * c;
+
+    printf("area: %f", a);
+
+}
